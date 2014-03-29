ebda5566bd80387cc4647e437ac4bec136d4e06a
thethp <todderick@gmail.com>
2014-03-29 12:09:15 -0400

    Make it so that on meatspace, the color marker takes up more space, making it easier to distinguish similar colors

--- whosthatmeat.js
+++ whosthatmeat.js
@@ -3,7 +3,8 @@ javascript:(function(){
     var fingerprint = $(this).attr('data-fingerprint');
     if(fingerprint != undefined) {
       var colorString = fingerprint.substring(0,6);
-      $(this).append('<div style="position:absolute;width:30px;height:30px;left:0;top:0;background-color:#'+colorString+'"></div>');
+      var height = (document.URL.indexOf('face') === -1) ? "101px;" : "30px";
+      $(this).append('<div style="position:absolute;width:30px;height:'+height+';left:0;top:0;background-color:#'+colorString+'"></div>');
     }
   });
 })();
