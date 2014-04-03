47050927340d1014bbb37d720a26e6a2ac6aad3e
chee rabbits <chee@snaek.org>
2014-04-03 12:27:09 +0100

    Always know who that meat is
    Adds a whoisthatmeat bar to new meats as they are added !

--- whosthatmeat.js
+++ whosthatmeat.js
@@ -1,10 +1,15 @@
 javascript:(function(){
-  $('li').each(function() {
-    var fingerprint = $(this).attr('data-fingerprint');
+  var whoaremeat = function() {
+    element = $(this);
+    var fingerprint = element.attr('data-fingerprint');
     if(fingerprint != undefined) {
       var colorString = fingerprint.substring(0,6);
       var height = (document.URL.indexOf('face') === -1) ? "101px;" : "30px";
-      $(this).append('<div style="position:absolute;width:30px;height:'+height+';left:0;top:0;background-color:#'+colorString+'"></div>');
+      element.append('<div style="position:absolute;width:30px;height:'+height+';left:0;top:0;background-color:#'+colorString+'"></div>');
     }
-  });
+  };
+
+  $('#chat-list').on("DOMNodeInserted", function(event) {
+    event.target.nodeName == 'LI' && whoaremeat.bind(event.target)();
+  }).find('li').each(whoaremeat);
 })();
