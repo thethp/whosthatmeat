62edccd06bc4028b0fadb172e99fcd1259b16f7b
thethp <todderick@gmail.com>
2014-05-01 15:17:30 -0400

    Making colors not take up such a large percentage of the screen, and making it toggleable

--- whosthatmeat.js
+++ whosthatmeat.js
@@ -1,15 +1,19 @@
 javascript:(function(){
+  $('body').toggleClass('whosthatmeat');
+  if(!$('body').hasClass('whosthatmeat')) {
+      console.log('Huh');
+    $('.whosthatMeatMarker').remove();
+  }
   var whoaremeat = function() {
     element = $(this);
     var fingerprint = element.attr('data-fingerprint');
-    if(fingerprint != undefined) {
+    if(fingerprint != undefined && $('body').hasClass('whosthatmeat')) {
       var colorString = fingerprint.substring(0,6);
       var height = (document.URL.indexOf('face') === -1) ? "101px;" : "30px";
-      element.append('<div style="position:absolute;width:30px;height:'+height+';left:0;top:0;background-color:#'+colorString+'"></div>');
+      element.append('<div class="whosthatMeatMarker" style="position:absolute;width:18px;height:'+height+';left:0;top:0;background-color:#'+colorString+'"></div>');
     }
   };
-
-  $('#chat-list').on("DOMNodeInserted", function(event) {
-    event.target.nodeName == 'LI' && whoaremeat.bind(event.target)();
-  }).find('li').each(whoaremeat);
+      $('#chat-list').on("DOMNodeInserted", function(event) {
+	      event.target.nodeName == 'LI' && whoaremeat.bind(event.target)();
+	  }).find('li').each(whoaremeat);
 })();
