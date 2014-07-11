b7586f4457d6fd35cb7cfc5e99f943a53906e7e0
thethp <todderick@gmail.com>
2014-07-11 14:49:46 -0400

    Updating whos that meat bookmarklet to toggle colors when clicked, making it easier to figure out who is eating

--- whosthatmeat.js
+++ whosthatmeat.js
@@ -1,19 +1,23 @@
 javascript:(function(){
-  $('body').toggleClass('whosthatmeat');
-  if(!$('body').hasClass('whosthatmeat')) {
-      console.log('Huh');
-    $('.whosthatMeatMarker').remove();
+  $('body').toggleClass('whosThatmeat');
+  if(!$('body').hasClass('whosThatmeat')) {
+    $('.whosThatMeatMarker').remove();
   }
+
   var whoaremeat = function() {
     element = $(this);
     var fingerprint = element.attr('data-fingerprint');
-    if(fingerprint != undefined && $('body').hasClass('whosthatmeat')) {
+    if(fingerprint != undefined && $('body').hasClass('whosThatmeat')) {
       var colorString = fingerprint.substring(0,6);
       var height = (document.URL.indexOf('face') === -1) ? "101px;" : "30px";
-      element.append('<div class="whosthatMeatMarker" style="position:absolute;width:18px;height:'+height+';left:0;top:0;background-color:#'+colorString+'"></div>');
+      element.append('<div class="whosThatMeatMarker" colorString='+colorString+' style="cursor:pointer;position:absolute;width:18px;height:'+height+';left:0;top:0;background-color:#'+colorString+'"></div>');
     }
   };
-      $('#chat-list').on("DOMNodeInserted", function(event) {
-	      event.target.nodeName == 'LI' && whoaremeat.bind(event.target)();
-	  }).find('li').each(whoaremeat);
+  $('#chat-list').on("DOMNodeInserted", function(event) {
+    event.target.nodeName == 'LI' && whoaremeat.bind(event.target)();
+  }).find('li').each(whoaremeat);
+
+  $('#chat-list').on('click', '.whosThatMeatMarker', function(event) {
+    $('.whosThatMeatMarker:not([colorString='+$(event.target).attr("colorString")+'])').toggle();
+  });
 })();
