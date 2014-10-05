3f93ab8a6b80a8d572638843c22d727a508970b3
thethp <todderick@gmail.com>
2014-10-05 14:21:51 -0400

    Update 1, basic colors working

--- whosthatmeat.js
+++ whosthatmeat.js
@@ -1,38 +1,37 @@
 javascript:(function(){
   document.body.classList.toggle('whosthatmeat');
   if(!document.body.classList.contains('whosThatmeat')) {
-      [].forEach.call(document.querySelectorAll('.whosThatMeatMarker'),removeMarker(e));
+      [].forEach.call(document.querySelectorAll('.whosThatMeatMarker'),removeMarker);
   }
 
-  var whoaremeat = function() {
-    element = this;
+  var whoaremeat = function(_element) {
+    element = _element;
     var fingerprint = element.getAttribute('data-fp');
-    if(fingerprint !== undefined && document.body.classList.contains('whosThatmeat')) {
+    if(fingerprint !== undefined && document.body.classList.contains('whosthatmeat')) {
       var colorString = fingerprint.substring(0,6);
-      var height = (document.URL.indexOf('face') === -1) ? "200px;" : "30px";
       var newWTM = document.createElement('div');
       newWTM.className = 'whosThatMeatMarker';
       newWTM.setAttribute('colorString',colorString);
       newWTM.style.cursor = 'pointer';
       newWTM.style.position = 'absolute';
       newWTM.style.width = '30px';
-      newWTM.style.height = height;
-      newWTM.style.left = '100%';
+      newWTM.style.height = '150px';
+      newWTM.style.left = '200px';
       newWTM.style.top = '0';
+      newWTM.style.zIndex = '-1';
       newWTM.style.background = '#'+colorString;
       element.appendChild(newWTM);
     }
   };
-  $('#chat-list').on("DOMNodeInserted", function(_event) {
-    _event.target.nodeName == 'LI' && whoaremeat.bind(_event.target)();
-  }).find('li').each(whoaremeat);
-
-  $('#chat-list').on('click', '.whosThatMeatMarker', function(_event) {
-    $('.whosThatMeatMarker:not([colorString='+$(_event.target).attr("colorString")+'])').toggle();
+  document.getElementById('messages').addEventListener('DOMNodeInserted', function(_event) {
+    _event.target.nodeName === 'li' && whoaremeat.bind(_event.target)();
   });
-  
+  [].forEach.call(document.querySelectorAll('#messages li'), function(_element){whoaremeat(_element);});
+
+  function toggleMarker(_event) {
+      [].forEach.call(document.querySelectorAll('.whosThatMeatMarker:not([colorString='+_event.target.getAttribute('colorString')+'])'),function(_element){console.log(_element);});
+  }
   function removeMarker(_element) {
-    e.parent.removeChild(_element);
+    _element.parent.removeChild(_element);
   }
 })();
-
